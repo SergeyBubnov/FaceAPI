d5e63bc5a52692efb93d8a6ea4e4f95b7f16e5b3
SergeyBubnov <53602534+SergeyBubnov@users.noreply.github.com>
2020-02-25 02:27:35 +0300

    Add files via upload

--- recognize.php
+++ recognize.php
@@ -10,6 +10,7 @@
 $filepath = htmlspecialchars($_POST['filepath']);
 $Server = $_SERVER['SERVER_NAME'];
 $fullpath = 'https://'.$Server.'/'.$filepath;
+$key = htmlspecialchars($_POST['key']);
 $endpoint = htmlspecialchars($_POST['endpoint']);
 
 //endpoint = "https://southcentralus.api.cognitive.microsoft.com";
@@ -38,6 +39,53 @@ print_r($array);
 echo '</pre>';
 curl_close($ch);
 ?>
+ 
+/*
+<?php
+
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2('https://southcentralus.api.cognitive.microsoft.com/face/v1.0/detect');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $key,
+);
+
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'returnFaceId' => 'true',
+    'returnFaceLandmarks' => 'false',
+    'returnFaceAttributes' => '{string}',
+    'recognitionModel' => 'recognition_01',
+    'returnRecognitionModel' => 'false',
+    'detectionModel' => 'detection_01',
+);
+
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body
+$request->setBody("{body}");
+
+try
+{
+    $response = $request->send();
+    echo $response->getBody();
+}
+catch (HttpException $ex)
+{
+    echo $ex;
+}
+
+?>
+ */
+
 </body>
 </html>
 
